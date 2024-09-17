9e332c8b4d2fb19c5c6c7d65679d46fcbea0c6c0
Saraswathi25 <saras.sapna25@gmail.com>
2024-09-17 18:57:16 -0400

    second

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-    <h1>HI</h1>
+    <h1>Hello</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
