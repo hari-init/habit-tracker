6f007123171efebde4b6921ab6263e447cbd7c43
Hari <140445632+hari-init@users.noreply.github.com>
2024-10-03 14:45:58 -0400

    Project restructure with backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const authRoute = require("./routes/authRoute");
+const userRoute = require("./routes/userRoute");
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
+app.use(express.json());
+
+//Routes
+app.use("/", authRoute);
+app.use("/", userRoute);
+
+//Listen
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
