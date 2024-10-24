46ac5b929d11e3ae62e849866388863c99f04681
Saraswathi25 <saras.sapna25@gmail.com>
2024-10-24 19:20:56 -0400

    alignment fix

--- frontend/src/components/textBox/TextBox.jsx
+++ frontend/src/components/textBox/TextBox.jsx
@@ -2,7 +2,7 @@ import { Icon } from '@iconify/react/dist/iconify.js';
 
 const TextBox = ({ userData, icon, label, onChange, readOnly = false }) => {
   return (
-    <div className='mt-6'>
+    <div className='mt-4 overflow-hidden'>
       <span className='text-sm block mb-1 text-left'>{label}</span>
       <label className='input input-bordered flex items-center gap-2'>
         <Icon icon={icon} className='opacity-70' width={30} height={30} />
