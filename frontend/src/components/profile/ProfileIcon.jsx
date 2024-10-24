468fe6931869e2aef99af2752aaa02f02e56d7fa
Saraswathi25 <saras.sapna25@gmail.com>
2024-10-24 19:05:50 -0400

    Edit Profile

--- frontend/src/components/profile/ProfileIcon.jsx
+++ frontend/src/components/profile/ProfileIcon.jsx
@@ -1,13 +1,26 @@
+import { Icon } from '@iconify/react/dist/iconify.js';
 
-const ProfileIcon =()=>{
-    return(
-        <div className="flex justify-center -mt-44">
-        <div className="avatar">
-        <div className="w-40 h-40 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 shadow-md">
-          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+const ProfileIcon = ({ profileImg, user, iconStyle, iconSize }) => {
+  return (
+    <div className={`${iconStyle} flex`}>
+      <div className='avatar'>
+        <div
+          className={`${iconSize} rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 shadow-md`}
+        >
+          <div>
+            {user ? (
+              <img src={profileImg} alt='ht logo' />
+            ) : (
+              <Icon
+                icon='solar:user-bold-duotone'
+                width={40}
+                style={{ padding: '4px' }}
+              />
+            )}
+          </div>
         </div>
       </div>
-      </div>
-    )
-}
+    </div>
+  );
+};
 export default ProfileIcon;
--- frontend/src/components/textBox/TextBox.jsx
+++ frontend/src/components/textBox/TextBox.jsx
@@ -1,9 +1,20 @@
-const TextBox = ({ Name }) => {
+import { Icon } from '@iconify/react/dist/iconify.js';
+
+const TextBox = ({ userData, icon, label, onChange, readOnly = false }) => {
   return (
-    <label className='input input-bordered flex items-center gap-2 mt-6'>
-      {Name}
-      <input type='text' className='grow'  />
-    </label>
+    <div className='mt-6'>
+      <span className='text-sm block mb-1 text-left'>{label}</span>
+      <label className='input input-bordered flex items-center gap-2'>
+        <Icon icon={icon} className='opacity-70' width={30} height={30} />
+        <input
+          type='text'
+          className='grow'
+          value={userData} // Controlled input
+          onChange={onChange} // Handler for updating state
+          readOnly={readOnly} // Set readOnly when required
+        />
+      </label>
+    </div>
   );
 };
 
