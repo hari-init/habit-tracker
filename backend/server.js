50a973c0a37295519327ffe1945d2b6226f42e1c
Hari <140445632+hari-init@users.noreply.github.com>
2024-10-29 15:40:43 -0400

    Create habit

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,24 @@ const cors = require("cors");
 require("dotenv").config();
 const authRoute = require("./routes/authRoute");
 const userRoute = require("./routes/userRoute");
+const habitRoute = require("./routes/habitRoute");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors({ 
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
- }));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 //Routes
 app.use("/", authRoute);
 app.use("/", userRoute);
+app.use("/", habitRoute);
 
 //Listen
 app.listen(port, () => {
